9b4d1279d33468248ee24a2b875e99f1e4ba9597
lifenglifeng001 <lifenglifeng001@gmail.com>
2014-12-24 17:45:48 +0800

    实现删除功能

--- list/list.c
+++ list/list.c
@@ -5,7 +5,6 @@
 
 
 /* 初始化列表 */
-
 list_t *list_init(int array[], int size)
 {
     int index;
@@ -31,18 +30,16 @@ list_t *list_init(int array[], int size)
 /* 在列表末尾添加元素 */
 void list_append(list_t *list, int value)
 {
-    node_t *node_append, *node_iter;
+    node_t *last_node, *node_append;
+    int list_length = list_len(list);
 
-    node_iter = list;
-    while (node_iter->next != NULL) {
-        node_iter = node_iter->next;
-    }
+    last_node = list_get_node(list, list_length - 1);
 
     node_append = malloc(sizeof(node_t));
     node_append->value = value;
     node_append->next = NULL;
 
-    node_iter->next = node_append;
+    last_node->next = node_append;
 }
 
 /* 打印列表 */
@@ -77,43 +74,87 @@ int list_len(list_t *list)
     return index;
 }
 
+/* 在指定索引处插入数据 */
 void list_insert(list_t *list, int index, int value)
 {
-    int i;
-    node_t *node_inserted;
-    node_t *node_iter = list;
-    for(i = 1; i < index; i++) {
-        node_iter = node_iter->next;
-    }
+    node_t *prev_node, *node_inserted;
 
+    prev_node = list_get_prev_node(list, index);
     node_inserted = malloc(sizeof(list_t));
     node_inserted->value = value;
-    node_inserted->next = node_iter->next;
-    node_iter->next=node_inserted;
+    node_inserted->next = prev_node->next;
+    prev_node->next=node_inserted;
 }
 
+
+/* 获取列表中指定索引对应的值*/
 int list_get(list_t *list, int index)
 {
-    int i = 1;
-    node_t *node_iter = list->next;
-    while (i < index && node_iter != NULL) {
-        node_iter = node_iter->next;
+    node_t * node;
+    node = list_get_node(list, index);
+    return node->value;
+}
+
+/* 获取列表中指定索引对应的node */
+node_t *list_get_node(list_t *list, int index)
+{
+    int i = 0;
+    node_t *node = list->next;
+    while (i < index && node != NULL) {
+        node = node ->next;
         i++;
     }
-    if (node_iter == NULL) {
+    if (node == NULL) {
         LOG_ERROR("Error: list index out of range!\n");
         exit(1);
     }
-    return node_iter->value;
+    return node;
 }
+
+/* 获取列表中指定索引上一个node */
+node_t *list_get_prev_node(list_t *list, int index)
+{
+    node_t *prev_node;
+    if (index == 0) {
+        prev_node = list;
+    } else {
+        prev_node = list_get_node(list, index-1);
+    }
+    return prev_node;
+}
+
+/* 删除列表中索引对应的node */
+void list_del_node(list_t *list, int index)
+{
+    node_t *node, *prev_node;
+    node = list_get_node(list, index);
+    prev_node = list_get_prev_node(list, index);
+    prev_node->next = node->next;
+    free(node);
+}
+
+/* 删除整个列表 */
+void free_list(list_t *list)
+{
+    node_t *node, *next_node;
+    node = list;
+
+    while (node != NULL) {
+        next_node = node->next;
+        free(node);
+        node = next_node;
+    }
+}
+
 
 int main(void) {
     list_t *ids;
     int values[] = {1, 2, 4, 5, 9};
     ids = list_init(values, 5);
-    list_insert(ids, 1, 99);
-    list_append(ids, 100);
     print_list(ids);
     printf("The length of list is %d\n", list_len(ids));
-    printf("The 2th value of list is %d\n", list_get(ids, 9));
+    list_del_node(ids, 1);
+    print_list(ids);
+    printf("The length of list is %d\n", list_len(ids));
+    free_list(ids);
 }
--- list/list.h
+++ list/list.h
@@ -1,7 +1,6 @@
 /*
  * list 头文件
  *
- * list 从1开始, 链表第一个为头
  * 其他尽量模仿python list
  */
 
@@ -18,8 +17,15 @@ typedef node_t list_t;
 
 /* list函数声明 */
 list_t *list_init(int arr[], int size);
-int list_len(list_t *);
-void list_append(list_t *, int);
+
+node_t *list_get_node(list_t *, int);
+node_t *list_get_prev_node(list_t *, int);
+
 void print_list(list_t *);
+int list_len(list_t *);
+
+void list_append(list_t *, int);
+void list_insert(list_t *, int, int);
 int list_get(list_t *, int);
 
+void list_delete(list_t *, int);
