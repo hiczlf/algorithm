1540eac93e007f61a33a1fa6305485fc36611db1
lifenglifeng001 <lifenglifeng001@gmail.com>
2015-03-22 18:08:00 +0800

    重构链表实现栈

--- stack/list_i/stack.c
+++ stack/list_i/stack.c
@@ -1,40 +1,44 @@
+/* 链表实现的栈 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include "stack.h"
 
-typedef struct _node {
-    int value;
-    struct _node *next;
-} node_t, stack_t;
-
-
-node_t *create_node(int value) {
-    node_t *node = malloc(sizeof(node_t));
-    node->value = value;
-    return node;
+struct stack_t *stack_init() {
+    struct stack_t *stack = malloc(sizeof(struct stack_t));
+    stack->top = NULL;
+    return stack;
 }
 
-stack_t *push(int value, stack_t **stack) {
-    node_t *node_new = create_node(value);
-    node_new->next = *stack;
-    *stack = node_new;
-    return *stack;
+struct stack_node_t *create_stack_node(stack_elem_t value) {
+    struct stack_node_t *new_node = malloc(sizeof(struct stack_node_t));
+    new_node->value = value;
+    return new_node;
 }
 
-int pop(stack_t **stack) {
-    int pop_value;
-    node_t *node_first;
-    node_first = *stack;
-    pop_value = node_first->value;
+void push(stack_elem_t value, struct stack_t *stack) {
+    struct stack_node_t *new_node = create_stack_node(value);
+    new_node->next = stack->top;
+    stack->top = new_node;
+    return;
+}
 
-    *stack = (*stack)->next;
-    free(node_first);
+stack_elem_t pop(struct stack_t *stack) {
+    stack_elem_t pop_value;
+    struct stack_node_t *pop_node;
+
+    pop_node = stack->top;
+    pop_value = pop_node->value;
+
+    stack->top = stack->top->next;
+    free(pop_node);
     return pop_value;
 }
 
-void print_stack(stack_t *stack) {
+void print_stack(struct stack_t *stack) {
 
-    node_t *node;
-    node = stack;
+    struct stack_node_t *node;
+    node = stack->top;
     printf("[");
     while(node != NULL) {
        if (node->next == NULL) {
@@ -47,26 +51,10 @@ void print_stack(stack_t *stack) {
     printf("]\n");
 }
 
-int is_empty(stack_t *stack) {
+bool is_empty(struct stack_t *stack) {
     if (stack == NULL) {
         return 1;
     } else {
         return 0;
     }
 }
-
-
-int main(void) {
-    stack_t *stack = NULL;
-    push(100, &stack);
-    push(2, &stack);
-    push(1001, &stack);
-
-    printf("Pop value is %d\n", pop(&stack));
-    print_stack(stack);
-    printf("stack is empty: %d\n", is_empty(stack));
-    pop(&stack);
-    pop(&stack);
-    printf("stack is empty: %d\n", is_empty(stack));
-}
-
